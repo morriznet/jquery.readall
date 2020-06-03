9ace718f8aa5534853ad3713a0153100810ba206
Anders Fjällström <anders@morriz.net>
2020-06-03 23:06:37 +0200

    Ongoing work

--- js/jquery.readall.js
+++ js/jquery.readall.js
@@ -15,20 +15,32 @@
             {
                 textShowmore: 'Read more',  // text shown on button to show more
                 textShowless: 'Read less',  // text shown on button to show less
-                classShowmore: 'button',    // class(es) on button to show more
-                classShowless: 'button'     // class(es) on button to show less
+                classShowmore: 'readall-button',    // class(es) on button to show more
+                classShowless: 'readall-button'     // class(es) on button to show less
             }
         }, options);
         var $this = $(this),
             singleline = parseFloat($this.css("line-height")),
-            fullheight = $this.innerHeight(),
+            fullheight = function () { return $this.innerHeight(); },
             showheight = singleline * settings.linenumbers,
-            objWrapper = $('<div />').addClass('readall-wrapper');
-        $this.wrap(objWrapper);
+            wrapperclass = 'readall-wrapper',
+            lessclass = 'readall-show',
+            moreclass = 'readall-hide';
+        $this.addClass('readall');
 
         var onResize = function (event) { console.log('resize'); console.log(event); };
-        $(window).bind('orientationchange resize', onResize);
 
+        if ($this.parent().not(wrapperclass)) {
+            $this.wrap($('<div />').addClass(wrapperclass));
+            var _button = $('<button />').addClass(settings.button.classShowmore).text(settings.button.textShowmore).click(function (e) {
+                e.preventDefault();
+                //TODO: handle click
+            });
+            $this.after(_button);
+
+            $(window).bind('orientationchange resize', onResize);
+        }
+        
         return this;
     };
 }(jQuery));
