3e2d8b727c0e80d2fd09ca45985a340c1fbe3e50
Anders Fjällström <anders@morriz.net>
2020-06-04 23:38:26 +0200

    Work is still ongoing

--- js/jquery.readall.js
+++ js/jquery.readall.js
@@ -10,37 +10,53 @@
     $.fn.readall = function (options) {
         var settings = $.extend({
             // Default values
-            linenumbers: 3,                 // number of lines to show
-            button:                         // settings for buttons
+            showheight: 96,                         // height to show
+            button:                                 // settings for buttons
             {
-                textShowmore: 'Read more',  // text shown on button to show more
-                textShowless: 'Read less',  // text shown on button to show less
+                textShowmore: 'Read more',          // text shown on button to show more
+                textShowless: 'Read less',          // text shown on button to show less
                 classShowmore: 'readall-button',    // class(es) on button to show more
                 classShowless: 'readall-button'     // class(es) on button to show less
             }
         }, options);
-        var $this = $(this),
-            singleline = parseFloat($this.css("line-height")),
-            fullheight = function () { return $this.innerHeight(); },
-            showheight = singleline * settings.linenumbers,
-            wrapperclass = 'readall-wrapper',
-            lessclass = 'readall-show',
-            moreclass = 'readall-hide';
-        $this.addClass('readall');
+        $(this).each(function () {
+            var $this = $(this),
+                fullheight = function () { return $this[0].scrollHeight; /*$this.innerHeight();*/ },
+                wrapperclass = 'readall-wrapper',
+                hiddenclass = 'readall-hide';
+            $this.addClass('readall').css({ 'overflow': 'hidden' });
 
-        var onResize = function (event) { console.log('resize'); console.log(event); };
+            //console.log($this);
+            var onResize = function (event) { /*console.log('resize');*/ /*console.log(event);*/
+                var _button = $this.parent().find('button.' + settings.button.classShowmore + ', button.' + settings.button.classShowless);
+                if (fullheight() > settings.showheight) { $(_button).show(); } else { $(_button).hide(); }
+                //TODO: If button visibility is changed from load then the height will either be fixed or missing also the hiddenclass might differ from the state of the button
+                // -- make sure to set the correct values when state changes
+            };
+            
+            if ($this.parent().not(wrapperclass)) {
+                $this.wrap($('<div />').addClass(wrapperclass));
+                var _button = $('<button />').addClass(settings.button.classShowmore).text(settings.button.textShowmore).click(function (e) {
+                    e.preventDefault();
+                    if ($this.hasClass(hiddenclass)) {
+                        $this.css({ 'height': '', 'max-height': '' });
+                        $(this).text(settings.button.textShowless);
+                    } else {
+                        $this.css({ 'height': settings.showheight + 'px', 'max-height': settings.showheight + 'px' });
+                        $(this).text(settings.button.textShowmore);
+                    }
+                    $this.toggleClass(hiddenclass);
+                    $(this).toggleClass(settings.button.classShowmore).toggleClass(settings.button.classShowless);
+                });
+                $this.after(_button);
 
-        if ($this.parent().not(wrapperclass)) {
-            $this.wrap($('<div />').addClass(wrapperclass));
-            var _button = $('<button />').addClass(settings.button.classShowmore).text(settings.button.textShowmore).click(function (e) {
-                e.preventDefault();
-                //TODO: handle click
-            });
-            $this.after(_button);
-
-            $(window).bind('orientationchange resize', onResize);
-        }
-        
+                $(window).bind('orientationchange resize', onResize);
+                if (fullheight() > settings.showheight) {
+                    $this.addClass(hiddenclass).css({ 'height': settings.showheight + 'px', 'max-height': settings.showheight + 'px' });
+                }
+                onResize(null);
+            }
+        });
         return this;
     };
 }(jQuery));
